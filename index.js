a8c3972d1f76a6c566efe1e9539c6b0eda56a625
rrricardo00 <rrricardo00@yahoo.com.br>
2020-11-09 00:36:13 -0300

    aula

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/main', (req, res) => {
+  res.send('GET request to the mainpage')
+});
+
+app.get('/blog/:artigo?', (req, res) => {
+
+  const artigo = req.params.artigo;
+
+  artigo ? res.send('Artigo: ' + artigo) : res.send('Bem vindo ao meu blog');
+
+
+});
+
+app.get('/ola/:nome', (req, res) => {
+  const nome = req.params.nome;
+  res.send('ola, ' + nome)
+})
+
+app.listen(port, () => console.log(`started`));
